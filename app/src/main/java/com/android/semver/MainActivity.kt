16b9b836cf950873efd2a01e7a2349836aae66a9
talhafaki <talhafaki05@gmail.com>
2021-10-29 16:05:12 +0300

    add build number script and set text

--- app/src/main/java/com/android/semver/MainActivity.kt
+++ app/src/main/java/com/android/semver/MainActivity.kt
@@ -2,10 +2,16 @@ package com.android.semver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
+        val versionName = packageInfo?.versionName
+        findViewById<TextView>(R.id.version_name_tv).text = versionName
+
     }
 }
